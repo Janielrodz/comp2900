97d390f1bc977470d7c5a49af94a6adc10cf0662
Janiel Rodriguez <jrodriguez2992@arecibointer.edu>
2024-02-08 13:52:32 -0400

    update my files

--- modulo01/lab02/app.py
+++ modulo01/lab02/app.py
@@ -1,4 +1,6 @@
 mensaje = input("Entre su nombre: ")
-gpa = (input("Entre su GPA: "))
+gpa = float(input("Entre su GPA: "))
+edad = int(input("Entre su edad: "))
 
 print(f"Hola {mensaje}!!!, tu gpa es de {gpa}")
+print(f"Tu edad es de {edad} años")
