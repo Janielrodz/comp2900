36364e2f65f6faeef756b86b6600aa3b5cd00171
Janiel Rodriguez <jrodriguez2992@arecibointer.edu>
2024-02-29 14:53:09 -0400

    Update my files

--- modulo02/array1/app.py
+++ modulo02/array1/app.py
@@ -1,13 +1,16 @@
 import array
 
-aNumber = array.array('i')
+aNumber = [0] * 3
 
 for i in range(1, 4):
     number = int(input('Enter a number: '))
-    aNumber.append(number)
+    aNumber[i-1] = number
+    #
 
 for i in range(3): # from 0 to 4
-    print(f'Indice {i} - {aNumber[i]}')
+    print(f'Indice {i} - Valor {aNumber[i]}')
+
+aNumber.sort()
 
 for n in aNumber:
     print(f'Valor {n}')
