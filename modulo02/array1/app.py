7df2da2b1f39fd84621a5672695cd7c5daf3c47f
Janiel Rodriguez <jrodriguez2992@arecibointer.edu>
2024-02-29 14:27:09 -0400

    Update my files

--- modulo02/array1/app.py
+++ modulo02/array1/app.py
@@ -8,3 +8,9 @@ for i in range(1, 4):
 
 for i in range(3): # from 0 to 4
     print(f'Indice {i} - {aNumber[i]}')
+
+for n in aNumber:
+    print(f'Valor {n}')
+
+for i in range(len(aNumber)): #From 0 to 2
+    print(f'Indice {i} - valor {aNumber[i]}')
